d004c1c43954980365350f1e2a1c1a30ddb90212
Peyton <pmdavenport09@gmail.com>
2025-06-04 13:23:38 -0500

    Change name

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Peyton, world!")
+            Text("Peyton, Davenport!")
         }
         .padding()
     }

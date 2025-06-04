3ad3ff3f6beedae0581b233af818387c14c8531a
Peyton <pmdavenport09@gmail.com>
2025-06-04 14:01:13 -0500

    Added image to top of screen put caption at bottom and color in the middle

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "
+        VStack(alignment: .leading, spacing: 1.0) {
+            Image("Cheer")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+            Text("Peyton Davenport")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            Color(red: 0.3, green: 0.8, blue: 0.6)
+            Text("Cheer is a passion of mine and has been for about two years now. From the practices to the team building and bonding and even person growth learnig how to better myself are reasons towards this passion.")
+                .frame(height: 96.734)
+                
+                
+                
+                
+                
+                
+            
+            
+            
         }
-        .padding()
+        .accessibilityElement(children: /*@START_MENU_TOKEN@*/.contain/*@END_MENU_TOKEN@*/)
+        .accessibilityLabel(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/)
+        .accessibilityIdentifier(/*@START_MENU_TOKEN@*/"Identifier"/*@END_MENU_TOKEN@*/)
     }
 }
 

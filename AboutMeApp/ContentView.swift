38ec07af6f33f7a10f377a40673a58f0e5c6c7aa
Peyton <pmdavenport09@gmail.com>
2025-06-04 13:26:56 -0500

    Got rid of name and globe

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Peyton, Davenport!")
+            Image(systemName: "
         }
         .padding()
     }

b42d63d40aaabde945739c1829a4e9b5282c4067
Peyton <pmdavenport09@gmail.com>
2025-06-04 13:22:39 -0500

    Initial Commit

---
+++ AboutMeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AboutMeApp
+//
+//  Created by Scholar on 6/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Peyton, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
